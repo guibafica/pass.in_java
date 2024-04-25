8433fa52de4307f8b6ce96eafd4eb82484b0fd8b
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-04-25 10:49:13 -0400

    Initial commit

---
+++ src/test/java/guilhermebafica/com/br/passin/PassInApplicationTests.java
@@ -0,0 +1,13 @@
+package guilhermebafica.com.br.passin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
