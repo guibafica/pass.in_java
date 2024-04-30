3aa00ed884e3c7daa4ca104c5840f0a8072a4e00
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-04-30 10:42:25 -0400

    Add controllers routes for test

--- src/main/java/guilhermebafica/com/br/passin/controllers/AttendeeController.java
+++ src/main/java/guilhermebafica/com/br/passin/controllers/AttendeeController.java
@@ -1,9 +1,15 @@
 package guilhermebafica.com.br.passin.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Tell to Spring, that's a Controller
 @RequestMapping("/attendees") // EndPoint config
 public class AttendeeController {
+    @GetMapping
+    public ResponseEntity<String> getTest() {
+        return ResponseEntity.ok("Success (attendee)");
+    }
 }
--- src/main/java/guilhermebafica/com/br/passin/controllers/EventController.java
+++ src/main/java/guilhermebafica/com/br/passin/controllers/EventController.java
@@ -1,9 +1,15 @@
 package guilhermebafica.com.br.passin.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Tell to Spring, that's a Controller
 @RequestMapping("/events") // EndPoint config
 public class EventController {
+    @GetMapping
+    public ResponseEntity<String> getTest() {
+        return ResponseEntity.ok("Success (event)");
+    }
 }
