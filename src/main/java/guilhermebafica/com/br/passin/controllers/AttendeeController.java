20b1bc9e72297615c954ea3c75e65adb97c32f82
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-04-30 10:38:20 -0400

    Initialiazes the controllers

---
+++ src/main/java/guilhermebafica/com/br/passin/controllers/AttendeeController.java
@@ -0,0 +1,9 @@
+package guilhermebafica.com.br.passin.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Tell to Spring, that's a Controller
+@RequestMapping("/attendees") // EndPoint config
+public class AttendeeController {
+}
