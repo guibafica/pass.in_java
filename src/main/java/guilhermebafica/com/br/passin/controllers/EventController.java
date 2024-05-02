c3fe81378077881a08e9679bd3e85e1e5e81dabb
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-05-02 09:29:07 -0400

    Create get and post event endpoint

--- src/main/java/guilhermebafica/com/br/passin/controllers/EventController.java
+++ src/main/java/guilhermebafica/com/br/passin/controllers/EventController.java
@@ -1,13 +1,13 @@
 package guilhermebafica.com.br.passin.controllers;
 
+import guilhermebafica.com.br.passin.dto.event.EventIdDTO;
+import guilhermebafica.com.br.passin.dto.event.EventRequestDTO;
 import guilhermebafica.com.br.passin.dto.event.EventResponseDTO;
 import guilhermebafica.com.br.passin.services.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController // Tell to Spring, that's a Controller
 @RequestMapping("/events") // EndPoint config
@@ -19,5 +19,14 @@ public class EventController {
         EventResponseDTO event = this.service.getEventDetail(eventId);
 
         return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder) {
+        EventIdDTO eventIdDTO = this.service.createEvent(body);
+
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
+
+        return ResponseEntity.created(uri).body(eventIdDTO);
     }
 }
