4194c319f6f9bd274e1d1e96eb44b7c80609b526
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-05-06 11:52:37 -0400

    Create checkIn method

---
+++ src/main/java/guilhermebafica/com/br/passin/services/CheckInService.java
@@ -0,0 +1,35 @@
+package guilhermebafica.com.br.passin.services;
+
+import guilhermebafica.com.br.passin.domain.attendee.Attendee;
+import guilhermebafica.com.br.passin.domain.checkin.CheckIn;
+import guilhermebafica.com.br.passin.domain.checkin.exceptions.CheckInAlreadyExistsException;
+import guilhermebafica.com.br.passin.repositories.CheckinRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CheckInService {
+    private final CheckinRepository checkinRepository;
+
+    public void registerCheckIn(Attendee attendee) {
+        this.verifyCheckInExists(attendee.getId());
+
+        CheckIn newCheckIn = new CheckIn();
+
+        newCheckIn.setAttendee(attendee);
+        newCheckIn.setCreatedAt(LocalDateTime.now());
+
+        this.checkinRepository.save(newCheckIn);
+    }
+
+    private void verifyCheckInExists(String attendeeId) {
+        Optional<CheckIn> isCheckedIn = this.checkinRepository.findByAttendeeId(attendeeId);
+
+        if (isCheckedIn.isPresent()) throw new CheckInAlreadyExistsException("Attendee already checked in");
+    }
+}
